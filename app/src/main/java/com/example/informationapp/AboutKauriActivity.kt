d0622defa9e99522daa511d64e41ae6a859c5029
Jeet Khoda <152601662+JeAtKhode@users.noreply.github.com>
2025-06-07 22:18:03 +1200

    lang change
    Added string files for mauri transaltion
    added about kauri page

---
+++ app/src/main/java/com/example/informationapp/AboutKauriActivity.kt
@@ -0,0 +1,11 @@
+package com.example.informationapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AboutKauriActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.about_kauri)
+    }
+}
